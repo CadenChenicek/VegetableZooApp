fd28114c72b293b3625a9509804a6add9af8af03
antoniobandalo <bandalo1358@studnets.d211.org>
2019-10-17 11:13:56 -0500

    splash screens, main menu, working on game timer.

--- app/src/main/java/com/example/vegetablezooapp/GamePlayActivity.java
+++ app/src/main/java/com/example/vegetablezooapp/GamePlayActivity.java
@@ -7,15 +7,46 @@ import android.os.CountDownTimer;
 import android.widget.TextView;
 
 public class GamePlayActivity extends AppCompatActivity {
+    private static final long START_TIME_IN_MILLIS = 600000;
 
     private TextView countdownText;
     private CountDownTimer countDownTimer;
-    private long timeLeftInMilliseconds = 60000;
+    private long timeLeftInMillis = START_TIME_IN_MILLIS;
     private boolean timeRunning;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_play);
+
+        countdownText = findViewById(R.id.countdownText);
+
+
     }
+
+    private void startTimer(){
+        countDownTimer = new CountDownTimer(timeLeftInMillis, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                timeLeftInMillis = millisUntilFinished;
+                updateCountDownText();
+            }
+
+            @Override
+            public void onFinish() {
+
+            }
+        }.start();
+
+        timeRunning = true;
+    }
+
+    private void updateCountDownText(){
+        int minutes = (int) (timeLeftInMillis / 1000) / 60;
+        int seconds = (int) (timeLeftInMillis / 1000) % 60;
+
+        String timeLeftFormatted = String.format("%02d:%02d", minutes, seconds);
+    }
+
+
 }
